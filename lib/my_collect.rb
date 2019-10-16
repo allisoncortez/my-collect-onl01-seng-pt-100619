20e47f66dee1fb1858862cf1e0b20ec75900ba25
allison cortez <allizetorc@gmail.com>
2019-10-16 19:13:49 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,9 @@
 def my_collect(array)
   i = 0
-  new_collection = []
   while array < array.length 
-  new_collection << yield(array[i])
+  yield(array[i])
   i += 1
 end
-new_collection
+array
 end
 
